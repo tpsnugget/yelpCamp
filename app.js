2c38a4ddcd4de9d7b3c7b63d9c7050064aa072d7
Michael Giebner <mikegiebner@gmail.com>
2019-10-15 12:35:51 -0400

    # C1:33:324 - 334: Colt Steele's Web Dev Bootcamp # C1:33:334: YelpCamp: Comments: YelpCamp: Styling Comments Pt. 2    Added public and then public/stylesheets directories    Added main.css

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ mongoose.connect('mongodb://localhost:27017/yelp_camp', options)
 app.set('view engine', 'ejs')
 
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(__dirname + "/public"))
 
 //================
 //    ROUTES
