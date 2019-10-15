1a8d58514098dba71cbff779f90f33592305e155
Michael Giebner <mikegiebner@gmail.com>
2019-10-15 10:30:07 -0400

    # C1:33:324 - 334: Colt Steele's Web Dev Bootcamp # C1:33:330: YelpCamp: Comments: YelpCamp: Comments Pt. 1    - Nested ROUTES    - Added views/comments directory       - Added a new.ejs file to this directory    - Added views/campgrounds directory       - Moved index.ejs, new.ejs, show.ejs to this new directory       - Had to add ../ to the beginning of partials/header and footer since the          files have now been moved

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.get('/', (req, res) => {
 app.get('/campgrounds', (req, res) => {
    Campground.find({}, (err, campgrounds) => {
       if (err) {console.log(err)}
-      else {res.render('index', { campgrounds: campgrounds })}
+      else {res.render('campgrounds/index', { campgrounds: campgrounds })}
    })
 })
 
@@ -52,7 +52,7 @@ app.post('/campgrounds', (req, res) => {
 
 // New Route      /campgrounds/new  GET   Displays for to make a new campground
 app.get('/campgrounds/new', (req, res) => {
-   res.render('new.ejs')
+   res.render('campgrounds/new')
 })
 
 // Show Route     /campgrounds/:id         GET   Shows info about one campground
@@ -60,14 +60,20 @@ app.get('/campgrounds/:id', (req, res) => {
    Campground.findById(req.params.id).populate("comments").exec((err, foundCampground) => {
       if(err) {console.log(err)}
       else {
-         console.log(foundCampground)
-         res.render('show', {campground: foundCampground})
+         res.render('campgrounds/show', {campground: foundCampground})
       }
    })
 })
 
-//================
-//    ROUTES
-//================
+//=======================
+//    COMMENTS ROUTES
+//=======================
+
+app.get('/campgrounds/:id/comments/new', (req, res) => {
+   Campground.findById(req.params.id, (err, campground) => {
+      if (err) {console.log(err)}
+      else {res.render('comments/new', {campground: campground})}
+   })
+})
 
 app.listen(3000, process.env.IP, () => { console.log('The yelpCamp Server is running!!') })
