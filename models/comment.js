f37e620ac180a0c2ea5e67d91e8212cb7be5d772
Michael Giebner <mikegiebner@gmail.com>
2019-10-16 16:43:14 -0400

    # C1:36:347 - 349: Colt Steele's Web Dev Bootcamp # C1:36:347: YelpCamp: Cleaning Up: User Associations: Comment    - Associate users and comments    - Save author name to a comment automatically    - Deleted all campgrounds from the DB and then commented out the seedDB() function    - Removed the author section from the comment FORM

--- models/comment.js
+++ models/comment.js
@@ -1,8 +1,21 @@
 var mongoose = require("mongoose")
 
+// OLD OLD OLD OLD
+// var commentSchema = mongoose.Schema({
+//    text:    String,
+//    author:  String
+// })
+
+// Installed in C1:36:348
 var commentSchema = mongoose.Schema({
-   text:    "",
-   author:  ""
+   text: String,
+   author: {
+      id: {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "User"
+      },
+      username: String
+   }
 })
 
 module.exports = mongoose.model("Comment", commentSchema)
